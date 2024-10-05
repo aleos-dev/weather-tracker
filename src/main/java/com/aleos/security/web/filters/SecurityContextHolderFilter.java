b38d02dfe13312f79b406fbcc86f78cc0bde4f36
Aleksei Os. <oal.aleos@gmail.com>
2024-10-05 14:50:52 +0300

    feat: add SecurityContextHolderFilter to manage security context in requests

---
+++ src/main/java/com/aleos/security/web/filters/SecurityContextHolderFilter.java
@@ -0,0 +1,28 @@
+package com.aleos.security.web.filters;
+
+import com.aleos.security.web.context.SecurityContext;
+import com.aleos.security.web.context.SecurityContextHolder;
+import com.aleos.security.web.context.SecurityContextRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+
+@AllArgsConstructor
+public class SecurityContextHolderFilter extends HttpFilter {
+
+    private final SecurityContextRepository securityContextRepository;
+
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws ServletException, IOException {
+        Supplier<SecurityContext> contextSupplier = securityContextRepository.loadDeferredContext(req);
+        SecurityContextHolder.setDeferredContext(contextSupplier);
+        chain.doFilter(req, res);
+    }
+}
