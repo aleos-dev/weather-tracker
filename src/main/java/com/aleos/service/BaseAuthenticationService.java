fdadb0484dfa3b0ebc34a6f1baf4ea4e46a5d0ff
Aleksei Os. <oal.aleos@gmail.com>
2024-10-05 15:04:21 +0300

    feat: add AuthenticationService and BaseAuthenticationService

---
+++ src/main/java/com/aleos/service/BaseAuthenticationService.java
@@ -0,0 +1,31 @@
+package com.aleos.service;
+
+import com.aleos.exception.AuthenticationException;
+import com.aleos.repository.UserRepository;
+import com.aleos.security.core.Authentication;
+import com.aleos.security.core.AuthenticationToken;
+import com.aleos.security.core.Role;
+import com.aleos.security.core.SimpleGrantedAuthority;
+import com.aleos.security.encoder.PasswordEncoder;
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
+public class BaseAuthenticationService implements AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(String username, String password) throws AuthenticationException {
+        var user = userRepository.find(username)
+                .orElseThrow(() -> new AuthenticationException("The user does not exist"));
+
+        if (passwordEncoder.matches(password, user.getPassword())) {
+            return new AuthenticationToken(username, password, new SimpleGrantedAuthority(Role.USER));
+        }
+
+        throw new AuthenticationException("The password is incorrect");
+    }
+}
