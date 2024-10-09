00beff4e5152ed8e9f0b3bed242c8cddc63e54c5
Aleksei Os. <oal.aleos@gmail.com>
2024-10-09 18:42:23 +0300

    refactor: update UserRepository to handle NoResultException

--- src/main/java/com/aleos/repository/UserRepository.java
+++ src/main/java/com/aleos/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.aleos.repository;
 
 import com.aleos.model.entity.User;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 
 import java.util.Optional;
@@ -12,6 +15,16 @@ public class UserRepository {
     private final EntityManagerFactory emf;
 
     public Optional<User> find(String username) {
-        return Optional.of(emf.createEntityManager().find(User.class, username));
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class);
+            query.setParameter("username", username);
+            User user = query.getSingleResult();
+            return Optional.ofNullable(user);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        } finally {
+            em.close();
+        }
     }
 }
