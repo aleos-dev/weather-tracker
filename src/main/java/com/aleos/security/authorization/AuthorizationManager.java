2094e2ec3a0c337bc758977aed882ea44ac9ce4c
Aleksei Os. <oal.aleos@gmail.com>
2024-10-09 10:32:27 +0300

    feat: introduce support for capturing original requests, enhance authentication flow

--- src/main/java/com/aleos/security/authorization/AuthorizationManager.java
+++ src/main/java/com/aleos/security/authorization/AuthorizationManager.java
@@ -1,9 +1,13 @@
 package com.aleos.security.authorization;
 
+import com.aleos.exception.context.AuthenticationException;
+import com.aleos.exception.security.AccessDeniedException;
+import com.aleos.http.CustomHttpSession;
 import com.aleos.security.core.Authentication;
 import com.aleos.security.core.GrantedAuthority;
 import com.aleos.security.core.Role;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.List;
@@ -12,30 +16,45 @@ import java.util.function.Supplier;
 
 public class AuthorizationManager {
 
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(AuthorizationManager.class);
+
     private final Map<String, List<Role>> authorizationRules = new HashMap<>();
 
     public boolean check(HttpServletRequest req, Supplier<Authentication> authentication) {
         var auth = authentication.get();
 
-        if (auth != null && auth.isAuthenticated()) {
-
-            var reqUri = req.getRequestURI();
-            List<Role> allowedRoles = authorizationRules.get(reqUri);
-
-            // if not specified, access allowed
-            if (allowedRoles == null) {
-                return true;
-            }
-
-            return auth.getAuthorities().stream()
-                    .map(GrantedAuthority::getAuthority)
-                    .anyMatch(allowedRoles::contains);
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new AuthenticationException("Not authenticated");
         }
 
-        return false;
+        var reqUri = req.getRequestURI();
+        List<Role> allowedRoles = authorizationRules.get(reqUri);
+
+        if (allowedRoles == null) {
+            String authorizationRulesError = "Authorization rules not founded to the target resource";
+            logger.warn(authorizationRulesError);
+            throw new AccessDeniedException("Access denied");
+        }
+
+
+        boolean authDecision = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .anyMatch(allowedRoles::contains);
+
+        if (!authDecision && "anonymous".equals(auth.getPrincipal())) {
+            setOriginalRequestInSession(req);
+            throw new AuthenticationException("Authentication required");
+        }
+
+        return authDecision;
     }
 
     public void addRules(Map<String, List<Role>> rules) {
         authorizationRules.putAll(rules);
+    }
+
+    private static void setOriginalRequestInSession(HttpServletRequest req) {
+        var session = (CustomHttpSession) req.getAttribute(CustomHttpSession.SESSION_CONTEXT_KEY);
+        session.setOriginalRequest(req.getRequestURI());
     }
 }
